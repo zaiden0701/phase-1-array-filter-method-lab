96bbf9c6956d4f922be765839a6eddebef4e15ea
Aiden Zafar <+>
2022-09-28 18:50:43 -0400

    commit tree d044dcbad741579bc1f26a0b1f30f3b02bbb7b53

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, driver){
+    return drivers.filter((newDriver) => 
+    newDriver.toLowerCase().includes(driver.toLowerCase()))
+}
+
+// let newFuzzyDriver = []
+
+// function fuzzyMatch(drivers, driver) {
+//     if (newFuzzyDriver.includes(driver)){
+//         return drivers.filter(newFuzzyDriver)
+//     } else {
+//         return newFuzzyDriver
+//     }
+// }
+
+// function fuzzyMatch(drivers, letters) {
+//     return drivers.filter((sameLettersDriver) => 
+//     (sameLettersDriver.charAt(0)).includes(letters))
+// }
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => letters.charAt(0) === driver.charAt(0))
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver => name === driver.name)
+}
